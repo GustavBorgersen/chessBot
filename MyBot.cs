24b55e1e021d4ce07ebecefc71a5ab98c56f7831
GustavBorgersen <47574640+GustavBorgersen@users.noreply.github.com>
2023-08-18 15:53:03 +0200

    v3

--- MyBot.cs
+++ MyBot.cs
@@ -5,36 +5,66 @@ using System.Linq;
 
 public class MyBot : IChessBot
 {
-    Random rand = new Random();
+    Move startMove;
+    bool prioStartMove;
+    DateTime timeOut;
+
     public Move Think(Board board, Timer timer)
     {
         //Console.WriteLine("think");
 
         //int currentScore = Evaluate(board);
         //Console.WriteLine(currentScore);
+        //TimeSpan timeout = new TimeSpan(0, 0, 0, 0, 500);
+        int mate = 500000;
+        Tuple<Move, int> bestMove = alphaBeta(board, 1, -int.MaxValue, int.MaxValue, mate);
+        Move returnMove = bestMove.Item1;
 
-        DateTime startTime = DateTime.Now;
-        Tuple<Move, int> bestMove = alphaBeta(board, 5, -int.MaxValue, int.MaxValue); //NegaMax(board, 4);
+        int thinkTime = timer.MillisecondsRemaining / 60;
+
+        for (int i = 2; i < 20; i++)
+        {
+            //Console.WriteLine("Depth: " + i);
+            startMove = bestMove.Item1;
+            prioStartMove = true;
+            timeOut = DateTime.Now + new TimeSpan(0,0,0,0, thinkTime);
+            bestMove = alphaBeta(board, i, -int.MaxValue, int.MaxValue, mate);
+            if (DateTime.Now > timeOut)
+            {
+                Console.WriteLine("Break at depth: " + i);
+                break;
+            }
+            returnMove = bestMove.Item1;
+
+        }
+        
         //Console.WriteLine("Found move: " + bestMove.Item1.ToString());
         //Console.WriteLine("Time taken: " + (DateTime.Now - startTime).TotalMilliseconds);
-        return bestMove.Item1;
+        return returnMove;
     }
 
-    private Tuple<Move, int> alphaBeta(Board board, int depth, int a, int b)
+    private Tuple<Move, int> alphaBeta(Board board, int depth, int a, int b, int mate)
     {
         //Console.WriteLine("alphaBeta");
         Move bestMove = new Move();
         List<Move> moves = GetOrderedMoves(board);
-        if (depth == 0 || moves.Count == 0 || board.IsDraw())
+        if (depth == 0)
         {
             Tuple<Move, int> ret = new Tuple<Move, int>(bestMove, Evaluate(board));
+            return ret;
+        }
+
+        if (DateTime.Now > timeOut)
+        {
+            //Console.WriteLine("Timeout at depth " + depth);
+            Tuple<Move, int> ret = new Tuple<Move, int>(bestMove, 0);
             return ret;
         }
 
         foreach (Move move in moves)
         {
             board.MakeMove(move);
-            int value = -alphaBeta(board, depth - 1, -b, -a).Item2;
+            int value = -alphaBeta(board, depth - 1, -b, -a, mate - 1).Item2;
             board.UndoMove(move);
 
             if (value >= b)
@@ -46,6 +76,16 @@ public class MyBot : IChessBot
                 a = value;
                 bestMove = move;
             }
+
+        }
+
+        if (moves.Count == 0)
+        {
+            if (board.IsInCheck() == false || board.IsDraw())
+            {
+                return new Tuple<Move, int>(bestMove, 0);
+            }
+            return new Tuple<Move, int>(bestMove, -mate);
         }
 
         return new Tuple<Move, int>(bestMove, a);
@@ -59,6 +99,12 @@ public class MyBot : IChessBot
                       ThenBy(m => m.MovePieceType).
                       ToList();
 
+        
+        if (prioStartMove && moves.Remove(startMove)) 
+        {
+            moves = moves.Prepend(startMove).ToList();
+            prioStartMove = false;
+        }
         
 
         return moves;
